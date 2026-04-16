4dd87072612b627dac9d29a9cc23fffbbdab520d
Echefu Tobechukwu <tobex360@gmail.com>
2026-04-16 15:41:11 +0100

    feat: add global styling and navigation components
    - Added Navbar, Footer, and BackToTop components to App.jsx
    - Implemented global CSS styling with custom fonts and color scheme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing/Landing'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import BackToTop from './components/BackToTop'
 
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Landing />} />
       </Routes>
+      <Footer />
+      <BackToTop />
     </Router>
   )
 }
